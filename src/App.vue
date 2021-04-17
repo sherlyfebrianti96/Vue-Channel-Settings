066ebca908fe7be00e4ad0931716c9cc2eb5d114
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-04-18 01:13:11 +0200

    Install and configure the TailwindCss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -22,5 +23,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.sidebar {
+  width: 40%;
 }
 </style>
