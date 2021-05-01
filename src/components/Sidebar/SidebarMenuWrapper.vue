28477c04ffb765ef2f9e3967dac4c5f5dc38ea2c
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-05-01 21:59:59 +0200

    Move the list of Menus to the proper place

--- src/components/Sidebar/SidebarMenuWrapper.vue
+++ src/components/Sidebar/SidebarMenuWrapper.vue
@@ -30,15 +30,85 @@ import SidebarAction from "@/components/Sidebar/SidebarAction";
 export default {
   name: 'SidebarMenuWrapper',
   components: {SidebarAction, SidebarSubmenu, SidebarMenu},
-  props: {
-    menus: {
-      type: Array,
-      required: true,
+  data() {
+    return {
+      menus: [
+        {
+          name: 'Inbox',
+          expand: true,
+          children: [
+            {
+              name: 'New',
+              icon: 'fa-inbox',
+              badge: 17
+            },
+            {
+              name: 'Assigned',
+              icon: 'fa-user-friends',
+              badge: 110
+            },
+            {
+              name: 'Closed',
+              icon: 'fa-check',
+              badge: null
+            },
+            {
+              name: 'Spam',
+              icon: 'fa-exclamation-circle',
+              badge: null
+            }
+          ]
+        },
+        {
+          name: 'Personal',
+          expand: false,
+          children: []
+        },
+        {
+          name: 'Teams',
+          expand: false,
+          children: []
+        },
+        {
+          name: 'Channels',
+          expand: true,
+          children: [
+            {
+              name: 'New',
+              icon: 'fa-inbox'
+            },
+            {
+              name: 'Assigned',
+              icon: 'fa-user-friends'
+            },
+            {
+              name: 'Closed',
+              icon: 'fa-check'
+            },
+            {
+              name: 'Spam',
+              icon: 'fa-exclamation-circle'
+            }
+          ],
+          actions: [
+            {
+              name: 'Settings',
+              icon: 'fa-ellipsis-h',
+              emitter: 'channelSettings'
+            },
+            {
+              name: 'New Channel',
+              icon: 'fa-plus',
+              emitter: 'channelManagement'
+            }
+          ],
+        },
+      ]
     }
   },
   methods: {
     toggleMenuExpand(id) {
-      this.$emit('toggleMenuExpand', id);
+      this.menus[id].expand = !this.menus[id].expand;
     },
     async toggleSidebar() {
       await this.$store.dispatch('sidebarExpandToggle');
