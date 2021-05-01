17b2b1e562f8fef5ba749ffe8dbb2d49639f167e
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-05-01 19:44:48 +0200

    Toggle the sidebar on click the backdrop

--- src/components/Sidebar/SidebarContainer.vue
+++ src/components/Sidebar/SidebarContainer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="absolute top-0 w-full h-screen">
-    <div class="w-full h-full bg-gray-800 opacity-70 animate-backdrop" />
+    <div
+      class="w-full h-full bg-gray-800 opacity-70 animate-backdrop"
+      @click="toggleSidebar"
+    />
     <div class="fixed top-0 bg-white h-full w-10/12 md:w-6/12 lg:w-6/12 xl:w-2/12 animate-slide">
       <SidebarToggle class="right-2" />
 
@@ -101,6 +104,9 @@ export default {
   methods: {
     toggleMenuExpand(id) {
       this.menus[id].expand = !this.menus[id].expand;
+    },
+    async toggleSidebar() {
+      await this.$store.dispatch('toggleSidebarExpand');
     }
   },
 }
