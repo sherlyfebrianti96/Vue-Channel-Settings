85e422e65d76661c8e00adb9453a19574d38f01d
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-05-03 04:31:56 +0200

    Fixing modal size on mobile view

--- src/components/Modal/ModalWrapper.vue
+++ src/components/Modal/ModalWrapper.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="absolute top-0 w-full h-screen">
+  <div class="fixed top-0 w-full h-screen">
     <div
       class="w-full h-full bg-gray-800 opacity-70 animate-backdrop"
     />
-    <div class="modal fixed top-0 bg-white w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 left-1/5 lg:left-1/3 top-20 rounded-md p-5">
+    <div class="modal absolute top-0 bg-white w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 left-1/5 lg:left-1/3 rounded-md p-5">
       <h1 class="text-2xl mb-3">
         <strong>{{ header }}</strong>
       </h1>
@@ -29,6 +29,9 @@ export default {
 .modal {
   width: calc(100% * 10 / 12);
   left: calc((100% - (100% * 10 / 12)) / 2);
+  max-height: 90%;
+  top: 5%;
+  overflow: scroll;
 }
 
 @media (min-width: 768px) {
